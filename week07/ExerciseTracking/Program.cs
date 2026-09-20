f3f4f0702b65390234db76894a8ef5480b78abc7
thelmofaaoa <thelmofaaoa@gmail.com>
2026-09-21 09:19:42 +1000

    Complete Week 07 Exercise Tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running(
+            new DateTime(2022, 11, 3),
+            30,
+            4.8
+        );
+
+        Cycling cycling = new Cycling(
+            new DateTime(2022, 11, 3),
+            30,
+            9.7
+        );
+
+        Swimming swimming = new Swimming(
+            new DateTime(2022, 11, 3),
+            30,
+            40
+        );
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Running : Activity
+{
+    private double _distance;
+
+    public Running(DateTime date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+
+    protected override string GetActivityType()
+    {
+        return "Running";
+    }
+}
